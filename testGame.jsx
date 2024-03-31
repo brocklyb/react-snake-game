24fc1e5fdd89503282bb9c7dc373f6a0644014d8
Brock B <52468736+brocklyb@users.noreply.github.com>
2024-03-31 14:05:44 -0700

    Add files via upload
    Fixed gameboard color selection

--- testGame.jsx
+++ testGame.jsx
@@ -233,133 +233,133 @@ function handleKeyUp(event) {
       <button onClick={startApp}>Start Game</button>
    
         <div className="rows" id="column1">
-            <div className="cell" id="c1">1</div>
-            <div className="cell" id="c2">2</div>
-            <div className="cell" id="c3">3</div>
-            <div className="cell" id="c4">4</div>
-            <div className="cell" id="c5">5</div>
-            <div className="cell" id="c6">6</div>
-            <div className="cell" id="c7">7</div>
-            <div className="cell" id="c8">8</div>
-            <div className="cell" id="c9">9</div>
-            <div className="cell" id="c10">10</div>
+            <div className="cell" id="c1" style={{backgroundColor: gameBoardColor}}>1</div>
+            <div className="cell" id="c2" style={{backgroundColor: gameBoardColor}}>2</div>
+            <div className="cell" id="c3" style={{backgroundColor: gameBoardColor}}>3</div>
+            <div className="cell" id="c4" style={{backgroundColor: gameBoardColor}}>4</div>
+            <div className="cell" id="c5" style={{backgroundColor: gameBoardColor}}>5</div>
+            <div className="cell" id="c6" style={{backgroundColor: gameBoardColor}}>6</div>
+            <div className="cell" id="c7" style={{backgroundColor: gameBoardColor}}>7</div>
+            <div className="cell" id="c8" style={{backgroundColor: gameBoardColor}}>8</div>
+            <div className="cell" id="c9" style={{backgroundColor: gameBoardColor}}>9</div>
+            <div className="cell" id="c10" style={{backgroundColor: gameBoardColor}}>10</div>
         </div>
 
         <div className="rows" id="column2">
-            <div className="cell" id="c11">11</div>
-            <div className="cell" id="c12">12</div>
-            <div className="cell" id="c13">13</div>
-            <div className="cell" id="c14">14</div>
-            <div className="cell" id="c15">15</div>
-            <div className="cell" id="c16">16</div>
-            <div className="cell" id="c17">17</div>
-            <div className="cell" id="c18">18</div>
-            <div className="cell" id="c19">19</div>
-            <div className="cell" id="c20">20</div>
+            <div className="cell" id="c11" style={{backgroundColor: gameBoardColor}}>11</div>
+            <div className="cell" id="c12" style={{backgroundColor: gameBoardColor}}>12</div>
+            <div className="cell" id="c13" style={{backgroundColor: gameBoardColor}}>13</div>
+            <div className="cell" id="c14" style={{backgroundColor: gameBoardColor}}>14</div>
+            <div className="cell" id="c15" style={{backgroundColor: gameBoardColor}}>15</div>
+            <div className="cell" id="c16" style={{backgroundColor: gameBoardColor}}>16</div>
+            <div className="cell" id="c17" style={{backgroundColor: gameBoardColor}}>17</div>
+            <div className="cell" id="c18" style={{backgroundColor: gameBoardColor}}>18</div>
+            <div className="cell" id="c19" style={{backgroundColor: gameBoardColor}}>19</div>
+            <div className="cell" id="c20" style={{backgroundColor: gameBoardColor}}>20</div>
         </div>
 
         <div className="rows" id="column3">
-            <div className="cell" id="c21">21</div>
-            <div className="cell" id="c22">22</div>
-            <div className="cell" id="c23">23</div>
-            <div className="cell" id="c24">24</div>
-            <div className="cell" id="c25">25</div>
-            <div className="cell" id="c26">26</div>
-            <div className="cell" id="c27">27</div>
-            <div className="cell" id="c28">28</div>
-            <div className="cell" id="c29">29</div>
-            <div className="cell" id="c30">30</div>
+            <div className="cell" id="c21" style={{backgroundColor: gameBoardColor}}>21</div>
+            <div className="cell" id="c22" style={{backgroundColor: gameBoardColor}}>22</div>
+            <div className="cell" id="c23" style={{backgroundColor: gameBoardColor}}>23</div>
+            <div className="cell" id="c24" style={{backgroundColor: gameBoardColor}}>24</div>
+            <div className="cell" id="c25" style={{backgroundColor: gameBoardColor}}>25</div>
+            <div className="cell" id="c26" style={{backgroundColor: gameBoardColor}}>26</div>
+            <div className="cell" id="c27" style={{backgroundColor: gameBoardColor}}>27</div>
+            <div className="cell" id="c28" style={{backgroundColor: gameBoardColor}}>28</div>
+            <div className="cell" id="c29" style={{backgroundColor: gameBoardColor}}>29</div>
+            <div className="cell" id="c30" style={{backgroundColor: gameBoardColor}}>30</div>
         </div>
 
         <div className="rows" id="column4">
-            <div className="cell" id="c31">31</div>
-            <div className="cell" id="c32">32</div>
-            <div className="cell" id="c33">33</div>
-            <div className="cell" id="c34">34</div>
-            <div className="cell" id="c35">35</div>
-            <div className="cell" id="c36">36</div>
-            <div className="cell" id="c37">37</div>
-            <div className="cell" id="c38">38</div>
-            <div className="cell" id="c39">39</div>
-            <div className="cell" id="c40">40</div>
+            <div className="cell" id="c31" style={{backgroundColor: gameBoardColor}}>31</div>
+            <div className="cell" id="c32" style={{backgroundColor: gameBoardColor}}>32</div>
+            <div className="cell" id="c33" style={{backgroundColor: gameBoardColor}}>33</div>
+            <div className="cell" id="c34" style={{backgroundColor: gameBoardColor}}>34</div>
+            <div className="cell" id="c35" style={{backgroundColor: gameBoardColor}}>35</div>
+            <div className="cell" id="c36" style={{backgroundColor: gameBoardColor}}>36</div>
+            <div className="cell" id="c37" style={{backgroundColor: gameBoardColor}}>37</div>
+            <div className="cell" id="c38" style={{backgroundColor: gameBoardColor}}>38</div>
+            <div className="cell" id="c39" style={{backgroundColor: gameBoardColor}}>39</div>
+            <div className="cell" id="c40" style={{backgroundColor: gameBoardColor}}>40</div>
         </div>
 
         <div className="rows" id="column5">
-            <div className="cell" id="c41">41</div>
-            <div className="cell" id="c42">42</div>
-            <div className="cell" id="c43">43</div>
-            <div className="cell" id="c44">44</div>
-            <div className="cell" id="c45">45</div>
-            <div className="cell" id="c46">46</div>
-            <div className="cell" id="c47">47</div>
-            <div className="cell" id="c48">48</div>
-            <div className="cell" id="c49">49</div>
-            <div className="cell" id="c50">50</div>
+            <div className="cell" id="c41" style={{backgroundColor: gameBoardColor}}>41</div>
+            <div className="cell" id="c42" style={{backgroundColor: gameBoardColor}}>42</div>
+            <div className="cell" id="c43" style={{backgroundColor: gameBoardColor}}>43</div>
+            <div className="cell" id="c44" style={{backgroundColor: gameBoardColor}}>44</div>
+            <div className="cell" id="c45" style={{backgroundColor: gameBoardColor}}>45</div>
+            <div className="cell" id="c46" style={{backgroundColor: gameBoardColor}}>46</div>
+            <div className="cell" id="c47" style={{backgroundColor: gameBoardColor}}>47</div>
+            <div className="cell" id="c48" style={{backgroundColor: gameBoardColor}}>48</div>
+            <div className="cell" id="c49" style={{backgroundColor: gameBoardColor}}>49</div>
+            <div className="cell" id="c50" style={{backgroundColor: gameBoardColor}}>50</div>
         </div>
 
         <div className="rows" id="column6">
-            <div className="cell" id="c51">51</div>
-            <div className="cell" id="c52">52</div>
-            <div className="cell" id="c53">53</div>
-            <div className="cell" id="c54">54</div>
-            <div className="cell" id="c55">55</div>
-            <div className="cell" id="c56">56</div>
-            <div className="cell" id="c57">57</div>
-            <div className="cell" id="c58">58</div>
-            <div className="cell" id="c59">59</div>
-            <div className="cell" id="c60">60</div>
+            <div className="cell" id="c51" style={{backgroundColor: gameBoardColor}}>51</div>
+            <div className="cell" id="c52" style={{backgroundColor: gameBoardColor}}>52</div>
+            <div className="cell" id="c53" style={{backgroundColor: gameBoardColor}}>53</div>
+            <div className="cell" id="c54" style={{backgroundColor: gameBoardColor}}>54</div>
+            <div className="cell" id="c55" style={{backgroundColor: gameBoardColor}}>55</div>
+            <div className="cell" id="c56" style={{backgroundColor: gameBoardColor}}>56</div>
+            <div className="cell" id="c57" style={{backgroundColor: gameBoardColor}}>57</div>
+            <div className="cell" id="c58" style={{backgroundColor: gameBoardColor}}>58</div>
+            <div className="cell" id="c59" style={{backgroundColor: gameBoardColor}}>59</div>
+            <div className="cell" id="c60" style={{backgroundColor: gameBoardColor}}>60</div>
         </div>
 
         <div className="rows" id="column7">
-            <div className="cell" id="c61">61</div>
-            <div className="cell" id="c62">62</div>
-            <div className="cell" id="c63">63</div>
-            <div className="cell" id="c64">64</div>
-            <div className="cell" id="c65">65</div>
-            <div className="cell" id="c66">66</div>
-            <div className="cell" id="c67">67</div>
-            <div className="cell" id="c68">68</div>
-            <div className="cell" id="c69">69</div>
-            <div className="cell" id="c70">70</div>
+            <div className="cell" id="c61" style={{backgroundColor: gameBoardColor}}>61</div>
+            <div className="cell" id="c62" style={{backgroundColor: gameBoardColor}}>62</div>
+            <div className="cell" id="c63" style={{backgroundColor: gameBoardColor}}>63</div>
+            <div className="cell" id="c64" style={{backgroundColor: gameBoardColor}}>64</div>
+            <div className="cell" id="c65" style={{backgroundColor: gameBoardColor}}>65</div>
+            <div className="cell" id="c66" style={{backgroundColor: gameBoardColor}}>66</div>
+            <div className="cell" id="c67" style={{backgroundColor: gameBoardColor}}>67</div>
+            <div className="cell" id="c68" style={{backgroundColor: gameBoardColor}}>68</div>
+            <div className="cell" id="c69" style={{backgroundColor: gameBoardColor}}>69</div>
+            <div className="cell" id="c70" style={{backgroundColor: gameBoardColor}}>70</div>
         </div>
 
         <div className="rows" id="column8">
-            <div className="cell" id="c71">71</div>
-            <div className="cell" id="c72">72</div>
-            <div className="cell" id="c73">73</div>
-            <div className="cell" id="c74">74</div>
-            <div className="cell" id="c75">75</div>
-            <div className="cell" id="c76">76</div>
-            <div className="cell" id="c77">77</div>
-            <div className="cell" id="c78">78</div>
-            <div className="cell" id="c79">79</div>
-            <div className="cell" id="c80">80</div>
+            <div className="cell" id="c71" style={{backgroundColor: gameBoardColor}}>71</div>
+            <div className="cell" id="c72" style={{backgroundColor: gameBoardColor}}>72</div>
+            <div className="cell" id="c73" style={{backgroundColor: gameBoardColor}}>73</div>
+            <div className="cell" id="c74" style={{backgroundColor: gameBoardColor}}>74</div>
+            <div className="cell" id="c75" style={{backgroundColor: gameBoardColor}}>75</div>
+            <div className="cell" id="c76" style={{backgroundColor: gameBoardColor}}>76</div>
+            <div className="cell" id="c77" style={{backgroundColor: gameBoardColor}}>77</div>
+            <div className="cell" id="c78" style={{backgroundColor: gameBoardColor}}>78</div>
+            <div className="cell" id="c79" style={{backgroundColor: gameBoardColor}}>79</div>
+            <div className="cell" id="c80" style={{backgroundColor: gameBoardColor}}>80</div>
         </div>
 
         <div className="rows" id="column9">
-            <div className="cell" id="c81">81</div>
-            <div className="cell" id="c82">82</div>
-            <div className="cell" id="c83">83</div>
-            <div className="cell" id="c84">84</div>
-            <div className="cell" id="c85">85</div>
-            <div className="cell" id="c86">86</div>
-            <div className="cell" id="c87">87</div>
-            <div className="cell" id="c88">88</div>
-            <div className="cell" id="c89">89</div>
-            <div className="cell" id="c90">90</div>
+            <div className="cell" id="c81" style={{backgroundColor: gameBoardColor}}>81</div>
+            <div className="cell" id="c82" style={{backgroundColor: gameBoardColor}}>82</div>
+            <div className="cell" id="c83" style={{backgroundColor: gameBoardColor}}>83</div>
+            <div className="cell" id="c84" style={{backgroundColor: gameBoardColor}}>84</div>
+            <div className="cell" id="c85" style={{backgroundColor: gameBoardColor}}>85</div>
+            <div className="cell" id="c86" style={{backgroundColor: gameBoardColor}}>86</div>
+            <div className="cell" id="c87" style={{backgroundColor: gameBoardColor}}>87</div>
+            <div className="cell" id="c88" style={{backgroundColor: gameBoardColor}}>88</div>
+            <div className="cell" id="c89" style={{backgroundColor: gameBoardColor}}>89</div>
+            <div className="cell" id="c90" style={{backgroundColor: gameBoardColor}}>90</div>
         </div>
 
         <div className="rows" id="column10">
-            <div className="cell" id="c91">91</div>
-            <div className="cell" id="c92">92</div>
-            <div className="cell" id="c93">93</div>
-            <div className="cell" id="c94">94</div>
-            <div className="cell" id="c95">95</div>
-            <div className="cell" id="c96">96</div>
-            <div className="cell" id="c97">97</div>
-            <div className="cell" id="c98">98</div>
-            <div className="cell" id="c99">99</div>
-            <div className="cell" id="c100">100</div>
+            <div className="cell" id="c91" style={{backgroundColor: gameBoardColor}}>91</div>
+            <div className="cell" id="c92" style={{backgroundColor: gameBoardColor}}>92</div>
+            <div className="cell" id="c93" style={{backgroundColor: gameBoardColor}}>93</div>
+            <div className="cell" id="c94" style={{backgroundColor: gameBoardColor}}>94</div>
+            <div className="cell" id="c95" style={{backgroundColor: gameBoardColor}}>95</div>
+            <div className="cell" id="c96" style={{backgroundColor: gameBoardColor}}>96</div>
+            <div className="cell" id="c97" style={{backgroundColor: gameBoardColor}}>97</div>
+            <div className="cell" id="c98" style={{backgroundColor: gameBoardColor}}>98</div>
+            <div className="cell" id="c99" style={{backgroundColor: gameBoardColor}}>99</div>
+            <div className="cell" id="c100" style={{backgroundColor: gameBoardColor}}>100</div>
         </div>
     </div>
 </div>
